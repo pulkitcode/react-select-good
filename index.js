1af56d72b84293991b0dc7be0916bcca9a497fe3
ParthArora <partharora.india@gmail.com>
2021-01-06 17:00:07 +0530

    ready to deploy

--- index.js
+++ index.js
@@ -31,6 +31,10 @@ app.use('/Electives', electivesRouter);
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
+    const path = require('path')
+    app.get("*", (req,res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build'))
+    })
 }
 
 app.listen(port, ()=>{
